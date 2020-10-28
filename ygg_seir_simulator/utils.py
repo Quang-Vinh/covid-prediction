a46ee40d310dbf6aaa413cc4eb713a6b5852768e
Vinh <qvinhdo@gmail.com>
2020-10-27 23:17:54 -0400

    Refactor code and move plotting functions to src

--- ygg_seir_simulator/utils.py
+++ ygg_seir_simulator/utils.py
@@ -1,8 +1,6 @@
 import datetime
 
 import numpy as np
-import pandas as pd
-import plotly.graph_objects as go
 
 from fixed_params import DATE_STR_FMT, DASH_REGIONS
 
@@ -42,64 +40,3 @@ def remove_space_region(region):
 def add_space_region(region):
     if region in DASH_REGIONS:
         return region
-
-
-def plot_predictions(mortality_pred: pd.DataFrame, title: str = ""):
-    """
-    Creates a time series plotly plot of the actual and predicted cumulative deaths.
-
-    Args:
-        mortality_pred (pd.DataFrame): Dataframe containing actual and predicted mortalities.
-        title (str): Title of plot. Defaults to ''
-
-    Returns:
-        [go.Figure]: Plotly figure
-    """
-
-    # Set figure layout
-    layout = go.Layout(
-        title=title,
-        xaxis=dict(title="Date"),
-        yaxis=dict(title="Cumulative Deaths"),
-        height=600,
-    )
-    fig = go.Figure(layout=layout)
-
-    # Add line for end of current known data
-    end_date = mortality_pred.query("not is_forecast")["date_death_report"].max()
-    fig.update_layout(
-        shapes=[
-            dict(
-                type="line",
-                yref="paper",
-                y0=0,
-                y1=1,
-                xref="x",
-                x0=end_date,
-                x1=end_date,
-            )
-        ]
-    )
-
-    # Trace for actual deaths
-    fig.add_trace(
-        go.Scatter(
-            x=mortality_pred["date_death_report"],
-            y=mortality_pred["cumulative_deaths"],
-            mode="lines",
-            name="Actual",
-        )
-    )
-
-    # Trace for predicted deaths
-    fig.add_trace(
-        go.Scatter(
-            x=mortality_pred["date_death_report"],
-            y=mortality_pred["cumulative_deaths_pred"],
-            mode="lines",
-            line={"dash": "dash"},
-            name="Predicted",
-        )
-    )
-
-    return fig
